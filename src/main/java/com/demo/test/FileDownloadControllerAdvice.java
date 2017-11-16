cb43c09eeb2ca88c0247a8766429c52e23168409
maihuuloi123 <huuloi101629@gmail.com>
2017-11-16 11:29:32 +0700

    Remove unnecessary 'extends ResponseEntityExceptionHandler' in controller advice

--- src/main/java/com/demo/test/FileDownloadControllerAdvice.java
+++ src/main/java/com/demo/test/FileDownloadControllerAdvice.java
@@ -4,12 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice(basePackageClasses = FileDownloadController.class)
-public class FileDownloadControllerAdvice extends ResponseEntityExceptionHandler
+public class FileDownloadControllerAdvice
 {
 
   @ExceptionHandler({Exception.class})
